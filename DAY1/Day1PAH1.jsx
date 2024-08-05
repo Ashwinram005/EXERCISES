3acf7ec25136ae6c0284790e195a506b2ebd8331
Ashwinram005 <150768045+Ashwinram005@users.noreply.github.com>
2024-08-05 19:13:30 +0530

    Add files via upload

---
+++ DAY1/Day1PAH1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+    return (
+        <div className='App'>
+            <p>This is Team Functional Component</p>
+        </div>
+    );
+};
+
+export default App;
