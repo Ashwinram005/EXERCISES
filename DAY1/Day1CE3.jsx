cfcb0feb0c8966afda311d191ca23275f227d616
Ashwinram005 <150768045+Ashwinram005@users.noreply.github.com>
2024-08-05 19:14:01 +0530

    Add files via upload

---
+++ DAY1/Day1CE3.jsx
@@ -0,0 +1,10 @@
+import React,{useState} from 'react';
+import './App.css';
+ const App=()=>{
+  return(
+    <div className='App'>
+      <p>This is World Functional Component</p>
+    </div>
+  )
+}
+export default App;
